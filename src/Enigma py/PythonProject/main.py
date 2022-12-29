29b976db6566520d8f12b834bd69f10b13653e77
melika shirian <106620989+emika12@users.noreply.github.com>
2022-12-30 00:47:51 +0330

    all classes

--- src/Enigma py/PythonProject/main.py
+++ src/Enigma py/PythonProject/main.py
@@ -1,22 +1,86 @@
-import serial
-import time
+# import serial
+# import time
+#
+#
+# ArduinoSerial = serial.Serial('com7', 9600)
+# time.sleep(2)
+# print(ArduinoSerial.readline())
+# print("Enter 1 to turn ON LED and 0 to turn OFF LED")
+#
+# while 1:
+#     var = input()
+#     print("you entered", var)
+#
+#     if (var == '1'):
+#         ArduinoSerial.write(str.encode('1'))
+#         print("LED turned ON")
+#         time.sleep(1)
+#
+#     if (var == '0'):
+#         ArduinoSerial.write(str.encode('0'))
+#         print("LED turned OFF")
+#         time.sleep(1)
+from collections import deque
 
-ArduinoSerial = serial.Serial('com8',9600)
-time.sleep(2)
-print(ArduinoSerial.readline())
-print ("Enter 1 to turn ON LED and 0 to turn OFF LED")
 
-while 1:
-    var = input()
-    print ("you entered", var)
 
-    if (var == '1'):
-        ArduinoSerial.write(str.encode('1'))
-        print ("LED turned ON")
-        time.sleep(1)
+class Code:
 
-    if (var == '0'):
-        ArduinoSerial.write(str.encode('0'))
-        print ("LED turned OFF")
-        time.sleep(1)
+    def __int__(self, date, rotor1, rotor2, rotor3, plugboard):
+        self.date = date
+        self.rotor1 = rotor1
+        self.rotor2 = rotor2
+        self.rotor3 = rotor3
+        self.plugboard = plugboard
+
+
+
+
+class MyMap:
+
+    def __init__(self):
+        self.table = []
+
+
+    def hash_function(self,keyCharacters, valueCharacters):
+
+        keyCharacters = list(keyCharacters)
+        valueCharacters = list(valueCharacters)
+
+
+        for i in range(len(keyCharacters)):
+            new_entry = Entry(keyCharacters[i], valueCharacters[i])
+            self.table.add(new_entry)
+
+
+        return self.table
+
+
+    def rotation(self):
+        self.table =deque(self.table)
+        #rotating it to the right one time
+        self.table.rotate(1)
+
+
+    def find_value(self , key):
+
+        for entry in self.table:
+            if entry.key == key:
+                return entry
+
+    def find_key(self, value):
+
+        for entry in self.table:
+            if entry.value == value:
+                return entry
+
+
+class Entry:
+
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+
+
 
